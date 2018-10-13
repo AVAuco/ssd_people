5959952a146d9791fdaac5b7ee436acc0d875d90
zacr0 <zacro.suarez@gmail.com>
2018-10-13 22:11:41 +0200

    Improved demo code.

--- ssd_people_demo.m
+++ ssd_people_demo.m
@@ -12,8 +12,13 @@ function ssd_people_demo(varargin)
 %   values are 'upperbody' and 'head'.
 %
 %   'confThr':: 0.3
-%   Minimun confidence score. Detections below this value will be filtered
-%   out.
+%   Minimum confidence score between 0 and 1.. Detections below this value 
+%   will be filtered out.
+%
+% Example:
+%   - ssd_people_demo('gpu',1) -> runs the upperbody detector using
+% the GPU available at index 1.
+%   - ssd_people_demo('model','head') -> runs the head detector on the CPU.
 %   
 opts.gpu = [];
 opts.model = 'upperbody';
@@ -29,12 +34,12 @@ images = {
 };
 
 % Load the network model
-modelPath = sprintf(opts.model, 'models/%s-detector.mat');
-net = Net(load(opts.modelPath));
+modelPath = sprintf('models/%s-detector.mat', opts.model);
+net = Net(load(modelPath));
 
 % Get predictions for each test image
 for i=1:length(images)
-    % Read image and adapt it to network's input size
+    % Read image and resize it to network's input size
     im = single(imread(images{i}));
     im = imresize(im, net.meta.normalization.imageSize(1:2));
 
@@ -54,9 +59,9 @@ for i=1:length(images)
     preds = preds(sortedIdx, :);
     
     % Extract the most confident predictions
-    numKeep = numel(find(preds(:,2) >= opts.confThr));
-    box = double(preds(1:numKeep,3:end));
-    confidence = preds(1:numKeep,2);
+    keep = find(preds(:,2) >= opts.confThr);
+    bboxes = double(preds(keep,3:end));
+    scores = preds(keep,2);
     
     % Return image to cpu for visualisation
     if numel(opts.gpu) > 0, im = gather(im); end
@@ -64,20 +69,20 @@ for i=1:length(images)
     % Diplay predictions
     figure;
     im = im / 255;
-    x = box(:,1) * size(im, 2);
-    y = box(:,2) * size(im, 1);
-    width = box(:,3) * size(im, 2) - x;
-    height = box(:,4) * size(im, 1) - y;
+    x = bboxes(:,1) * size(im, 2);
+    y = bboxes(:,2) * size(im, 1);
+    width = bboxes(:,3) * size(im, 2) - x;
+    height = bboxes(:,4) * size(im, 1) - y;
     rectangle = [x, y, width, height];
     im = insertShape(im, 'Rectangle', rectangle, 'LineWidth', 2, 'Color', 'yellow');
     str = {};
-    for j = 1:numKeep
-        str{j} = sprintf('%.2f', confidence(j));
+    for j = 1:length(keep)
+        str{j} = sprintf('%.2f', scores(j));
     end
     im = insertText(im, [x, y], str, 'FontSize', 11);
     imagesc(im);
-    title(sprintf('SSD-people predictions (top %d are displayed)', numKeep), ...
-                   'FontSize', 14);
+    title(sprintf('SSD-people predictions (top %d are displayed)', ...,
+        length(keep)), 'FontSize', 14);
     axis off;
     
     % Free up the GPU allocation
